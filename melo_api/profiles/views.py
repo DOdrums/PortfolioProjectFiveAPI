094b9586a719a64360ec6246b595e4295539a167
DOdrums <dirkornee@hotmail.com>
2023-01-10 12:28:08 +0100

    Rewrite profile views to use generics django views

--- melo_api/profiles/views.py
+++ melo_api/profiles/views.py
@@ -1,26 +1,17 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
-from django.http import Http404
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
-class ProfileDetail(APIView):
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            return Profile
-        except Profile.DoesNotExist:
-            raise Http404
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retreive or update a profile if you're the owner
+    """
 
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
