ef6cc7c9eaf3c1652e65865785d00d1dbd4a2757
DOdrums <dirkornee@hotmail.com>
2023-01-11 16:29:11 +0100

    Make changes to song model

--- melo_api/song/models.py
+++ melo_api/song/models.py
@@ -6,6 +6,6 @@ class Song(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    audio = models.FileField(upload_to="songs/", default="../rock_it_pycp3h")
+    audio = models.FileField(upload_to="songs/", default="songs/rock_it_pycp3h")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
