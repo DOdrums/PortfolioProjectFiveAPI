3d7453995d241b2c62528ffb0ab2240ea38a39b9
DOdrums <dirkornee@hotmail.com>
2023-01-11 16:41:37 +0100

    Change song model to use proper cloudinary storage

--- melo_api/song/models.py
+++ melo_api/song/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary_storage.storage import VideoMediaCloudinaryStorage
+from cloudinary_storage.validators import validate_video
 
 
 class Song(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    audio = models.FileField(upload_to="songs/", default="songs/rock_it_pycp3h")
+    audio = models.FileField(
+        upload_to="songs/",
+        storage=VideoMediaCloudinaryStorage(),
+        default="songs/rock_it_pycp3h",
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
