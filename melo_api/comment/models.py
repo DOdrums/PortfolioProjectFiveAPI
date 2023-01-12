c9afa96e380f9540f6f0f3b830ddf08600a7d3a8
DOdrums <dirkornee@hotmail.com>
2023-01-12 11:55:02 +0100

    Add comment model

--- melo_api/comment/models.py
+++ melo_api/comment/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from post.models import Post
+from song.models import Song
 
 
-# class Comment(models.Model):
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#     V
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
