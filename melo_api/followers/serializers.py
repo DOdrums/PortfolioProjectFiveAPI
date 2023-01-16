d2b3ce7442a6419814bcf95e92d8da27a4991e2c
DOdrums <dirkornee@hotmail.com>
2023-01-16 17:46:12 +0100

    Fix bug in followers serializers

--- melo_api/followers/serializers.py
+++ melo_api/followers/serializers.py
@@ -4,7 +4,7 @@ from .models import Follower
 
 class FollowerSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
-    followed = serializers.ReadOnlyField(source="owner.username")
+    followed = serializers.ReadOnlyField(source="followed.username")
 
     class Meta:
         model = Follower
