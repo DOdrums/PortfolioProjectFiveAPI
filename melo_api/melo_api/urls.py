5d459ebd3754e457ec321d9c8d364b74afcc83e5
DOdrums <dirkornee@hotmail.com>
2023-01-16 18:01:37 +0100

    Add dj rest api-auth for login functionality

--- melo_api/melo_api/urls.py
+++ melo_api/melo_api/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
     path("", include("profiles.urls")),
     path("", include("post.urls")),
     path("", include("song.urls")),
