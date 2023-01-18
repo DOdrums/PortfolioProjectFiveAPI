364173401118d7536ffe5f44eac36a66a70a46f9
DOdrums <dirkornee@hotmail.com>
2023-01-18 10:32:39 +0100

    Add likes app and like model

---
+++ melo_api/likes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from post.models import Post
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
