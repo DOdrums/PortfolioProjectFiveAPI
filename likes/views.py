c779b4a326b6ee7f26fc28459ecc501fdf215eb3
DOdrums <dirkornee@hotmail.com>
2023-05-18 17:36:32 +0200

    Change LikeList view to also Create likes

--- likes/views.py
+++ likes/views.py
@@ -1,12 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Like
 from .serializers import LikeSerializer
 from melo_api.permissions import IsOwnerOrReadOnly
 
 
-class LikeList(generics.ListAPIView):
+class LikeList(generics.ListCreateAPIView):
+    """
+    List all likes or create a like if a user is logged in
+    The perform_create method associates the like with the logged in user.
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
