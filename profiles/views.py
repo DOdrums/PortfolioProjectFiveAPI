d7ecb411ed98320cafaa0f09bca18b6d32a4750d
DOdrums <dirkornee@hotmail.com>
2023-05-31 13:22:32 +0200

    Add post_count and song_count to profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -6,13 +6,21 @@ from melo_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by Django signals.
+    """
+
     queryset = Profile.objects.annotate(
         posts_count=Count("owner__post", distinct=True),
         songs_count=Count("owner__song", distinct=True),
-    )
+    ).order_by("-date_joined")
     serializer_class = ProfileSerializer
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ["posts_count", "songs_count"]
+    ordering_fields = [
+        "posts_count",
+        "songs_count",
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
@@ -21,5 +29,8 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
 
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        songs_count=Count("owner__song", distinct=True),
+    ).order_by("-date_joined")
     serializer_class = ProfileSerializer
