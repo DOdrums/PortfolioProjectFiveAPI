f9435a040d1d5c490b33036963e865d1b85f6ed4
DOdrums <dirkornee@hotmail.com>
2023-06-01 13:33:38 +0200

    Make follower list view a ListCreate view and add permission class

--- followers/views.py
+++ followers/views.py
@@ -1,12 +1,22 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Follower
 from .serializers import FollowerSerializer
 from melo_api.permissions import IsOwnerOrReadOnly
 
 
-class FollowerList(generics.ListAPIView):
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers: all instances of a user
+    following another user.
+    Create a follower with the perform_create function
+    """
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Follower.objects.all()
     serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class FollowerDetail(generics.RetrieveDestroyAPIView):
