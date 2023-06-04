d9ceb8b8ee146e7e0961908d5f9aa545af87a13a
DOdrums <dirkornee@hotmail.com>
2023-06-04 14:03:46 +0200

    Add instruments choices field to instrument

--- instruments/serializers.py
+++ instruments/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from .models import Instrument
+from .instruments import INSTRUMENTS
 
 
 class InstrumentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     instrument_name = serializers.SerializerMethodField()
     experience_name = serializers.SerializerMethodField()
+    instrument_choices = serializers.SerializerMethodField()
 
     def get_instrument_name(self, obj):
         return obj.get_instrument_display()
 
     def get_experience_name(self, obj):
         return obj.get_experience_display()
+
+    def get_instrument_choices(self, ojb):
+        return dict(INSTRUMENTS)
 
     class Meta:
         model = Instrument
@@ -23,4 +28,5 @@ class InstrumentSerializer(serializers.ModelSerializer):
             "experience",
             "experience_name",
             "created_at",
+            "instrument_choices",
         ]
